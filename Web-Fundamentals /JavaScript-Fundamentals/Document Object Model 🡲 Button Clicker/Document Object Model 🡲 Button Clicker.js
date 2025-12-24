837ce0cde6deadf888b9f75102d8dcb667596678
nizaralljamal <nizaralljamal@gmail.com>
2025-12-24 12:57:32 +0200

    Update files

---
+++ Web-Fundamentals /JavaScript-Fundamentals/Document Object Model 🡲 Button Clicker/Document Object Model 🡲 Button Clicker.js
@@ -0,0 +1,33 @@
+const mySubmit = document.getElementById("mySubmit");
+
+mySubmit.onclick = function() {
+    if (mySubmit.textContent === "Login") {
+        mySubmit.textContent = "Logout";
+    } else {
+        mySubmit.textContent = "Login";
+    }
+}
+const ninjaLike = document.getElementById("ninjaLike");
+ninjaLike.onclick = function(){
+    ninjaLike = window.alert("Ninja was liked")
+}
+
+const addDefinition = document.getElementById("addDefinition");
+
+addDefinition.onclick = function(){
+    addDefinition = addDefinition.remove();
+}
+
+   let count = 0;
+ const increaseBtn = document.getElementById("increaseBtn");
+
+ increaseBtn.onclick = function(){
+     count++;
+     increaseBtn.textContent = ` ${count} Likes`;
+ }
+
+ const myH1 = document.getElementById("myH1");
+
+ myH1.onclick = function(){
+    myH1.textContent = "Nizar Aljamal";
+ }
